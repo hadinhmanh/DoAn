81697068abb453a02fac15fbdeeebcdb20befbe4
hadinhmanh <108871275+hadinhmanh@users.noreply.github.com>
2025-03-17 16:13:45 +0700

    register

--- src/main/java/com/example/DoAn/controller/client/HomePageController.java
+++ src/main/java/com/example/DoAn/controller/client/HomePageController.java
@@ -1,15 +1,64 @@
 package com.example.DoAn.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.DoAn.controller.admin.DashBoardController;
+import com.example.DoAn.domain.User;
+import com.example.DoAn.domain.dto.RegisterDTO;
+import com.example.DoAn.service.UserService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class HomePageController {
 
+    private final UserService userService;
+
+    public HomePageController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String getHomePage() {
         return "client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    // @PostMapping("/register")
+    // public String handleRegister(
+    // @ModelAttribute("registerUser") @Valid RegisterDTO registerDTO,
+    // BindingResult bindingResult) {
+
+    // // validate
+    // if (bindingResult.hasErrors()) {
+    // return "client/auth/register";
+    // }
+
+    // User user = this.userService.registerDTOtoUser(registerDTO);
+
+    // String hashPassword = this.passwordEncoder.encode(user.getPassword());
+
+    // user.setPassword(hashPassword);
+    // user.setRole(this.userService.getRoleByName("USER"));
+    // // save
+    // this.userService.handleSaveUser(user);
+    // return "redirect:/login";
+
+    // }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "client/auth/login";
     }
 
 }
---
+++ src/main/java/com/example/DoAn/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.DoAn.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.DoAn.domain.User;
+import com.example.DoAn.domain.dto.RegisterDTO;
+import com.example.DoAn.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerDTOtoUser(RegisterDTO registerDTO) {
+        User user = new User();
+        user.setFullName(registerDTO.getFirstName() + " " + registerDTO.getLastName());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(registerDTO.getPassword());
+        return user;
+    }
+
+    public boolean checkEmailExist(String email) {
+        return this.userRepository.existsByEmail(email);
+    }
+}
